f4287d8a91b5318a29cd320d9749eb39c9eaa0d3
Tilen <prevolnik.tilen@gmail.com>
2017-11-26 00:33:07 +0100

    store/build image in/from array

--- assets/custom/js/ny-game.js
+++ assets/custom/js/ny-game.js
@@ -17,21 +17,31 @@ $(document).mousedown(function(e) {
 });
 
 // Add squares to the board
-function addSquares(numOfSquares) {
+function addSquares(squareNumber) {
     var square = '';
-    for (i = 1; i <= numOfSquares; i++) {
-        square += '<div class="ny-square"><div id=ny-cell-' + i + ' class="ny-cell ny-pink"></div></div>';
+    for (i = 1; i <= squareNumber; i++) {
+        square += '<div class="ny-square"><div id=ny-cell-' + i + ' class="ny-cell ny-pink">ny-pink</div></div>';
     }
     $('.ny-game-box').append(square);
 };
-addSquares(660);
+
+var numOfSquares = 400;
+addSquares(numOfSquares);
 
 // Button colors
 var lmbColor = 'ny-white';
 var rmbColor = 'ny-pink';
 
 // Change color of lmb and rmb
-$(".ny-color-btn").on("mousedown",function(e){
+// Change color of left mouse button
+$('.ny-color-btn').click(function() {
+    var pickedColor = $(this).html();
+    lmbColor = pickedColor;
+    $('.ny-lmb').attr('class', 'ny-menu-item ny-lmb').addClass(pickedColor);
+});
+
+// Change color of left and right mouse button
+/*$(".ny-color-btn").on("mousedown",function(e){
     var pickedColor = $(this).html();
     if( e.button == 2 ) {
       rmbColor = pickedColor;
@@ -40,7 +50,7 @@ $(".ny-color-btn").on("mousedown",function(e){
       lmbColor = pickedColor;
       $('.ny-lmb').attr('class', 'ny-menu-item ny-lmb').addClass(pickedColor);
     }
-});
+});*/
 
 // Change color of square
 // - when you drag your mouse
@@ -48,20 +58,39 @@ $(".ny-square").mouseover(function(){
     if(!isDown) {
         return false;
     } else if(isDown == 'rmb') {
-        $('div', this).attr('class', 'ny-cell').addClass(rmbColor);
+        $('div', this).attr('class', 'ny-cell').addClass(rmbColor).html(rmbColor);
     } else if (isDown == 'lmb') {
-        $('div', this).attr('class', 'ny-cell').addClass(lmbColor);
+        $('div', this).attr('class', 'ny-cell').addClass(lmbColor).html(lmbColor);
     }
 });
 // - when you click your mouse
 $(".ny-square").on("mousedown",function(e){
     if( e.button == 2 ) {
-      $('div', this).attr('class', 'ny-cell').addClass(rmbColor);
+      $('div', this).attr('class', 'ny-cell').addClass(rmbColor).html(rmbColor);
     } else {
-      $('div', this).attr('class', 'ny-cell').addClass(lmbColor);
+      $('div', this).attr('class', 'ny-cell').addClass(lmbColor).html(lmbColor);
     }
 });
 // - start from scratch
 $('.ny-refresh').click(function() {
-    $('.ny-square div').attr('class', 'ny-cell').addClass('ny-pink');
+    $('.ny-square div').attr('class', 'ny-cell').addClass('ny-pink').html('ny-pink');
+});
+
+// Store image in an array
+var nyImageArray= [];
+$('.ny-alert').click(function() {
+    for (i = 0; i < numOfSquares; i++) {
+        var cellColor = $('#ny-cell-' + (i + 1)).html();
+        nyImageArray.push(cellColor);
+    }
+});
+
+// Build image from array
+function buildImageFromArray(imgArray) {
+    for (i = 0; i < imgArray.length; i++) {
+        $('#ny-cell-' + (i + 1)).attr('class', 'ny-cell').addClass(imgArray[i]).html(imgArray[i]);
+    }
+}
+$('.ny-build').click(function() {
+    buildImageFromArray(nyImageArray);
 });
